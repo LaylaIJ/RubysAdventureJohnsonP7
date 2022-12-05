4905078b164bf70ed549edf35509910e2947acc5
LaylaIJ <116124238+LaylaIJ@users.noreply.github.com>
2022-12-05 15:27:20 -0800

    things fixed
    i fixed the tile map that was low gravity, added a new scrip to ruby. added script to the health collectible

--- RubysAdventureJohnsonP7/Assets/Scripts/RubyController.cs
+++ RubysAdventureJohnsonP7/Assets/Scripts/RubyController.cs
@@ -4,28 +4,45 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public float speed = 3.0f;
     
-    void Start()
-    {   
-   
+    public int maxHealth = 5;
+    int currentHealth;
+    
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
 
+
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D >();
+
+        currentHealth = maxHealth;
     }
 
-   
     void Update()
     {
-        //create variables to use the built in axis
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        //create variables to use the built in axis 
+        horizontal = Input.GetAxis("Horizontal");
+         vertical = Input.GetAxis("Vertical");
+    }
 
-        //create a movement vector
+    void FixedUpdate()
+    {   //create a movement vector
         Vector2 position = transform.position;
-
         //make horizontal and vertical movement work
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
 
-        transform.position = position;
+        rigidbody2d.MovePosition(position);
+    }
+     
+    
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
 
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
